fe050246d1c285f5953a8eb51eda9fdfe0e3bf69
Saviour Gidi <profsaviour@gmail.com>
2016-11-28 22:23:44 +0000

    improvements

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect, session, f
 from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, RoleMixin, login_required 
 from flask_sqlalchemy import SQLAlchemy
 from functools import wraps
+from models import *
 import datetime
 
 
@@ -9,55 +10,6 @@ import datetime
 app = Flask(__name__)
 app.config.from_object('config.BaseConfig')
 db = SQLAlchemy(app)
-
-
-#payments db 
-class PAYMENTS(db.Model):
-    __tablename__ = 'PAYMENTS'
-    id = db.Column(db.Integer, primary_key=True)
-    Account = db.Column(db.Integer)
-    GCR_No = db.Column(db.String(60))
-    Payments = db.Column(db.Integer)
-    PaymentType = db.Column(db.String(20))
-    PaidBy = db.Column(db.String(60))
-    PaidByTele = db.Column(db.String(10))
-    Cashier = db.Column(db.String(25))
-    Pc_name = db.Column(db.String(25))
-    DatePaid = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
-    DatePaid = db.Column(db.DateTime(), default=datetime.datetime.today())
-
-
-
-    def __init__(self, Account, GCR_No, Payments, PaymentType, PaidBy, PaidByTele, Cashier):
-        self.Account = Account
-        self.GCR_No = GCR_No
-        self.Payments = Payments
-        self.PaymentType = PaymentType
-        self.PaidBy = PaidBy
-        self.PaidByTele = PaidByTele
-        self.Pc_name = Pc_name
-        self.Cashier = Cashier
-
-    def __repr__(self):
-        return '<records %r>' % self.Account
-
-#users db
-class User(db.Model):
-    __tablename__ = 'User'
-    #__table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(10), unique=True)
-    password = db.Column(db.String(10), unique=True)
-
-
-    def __init__(self, username, password):
-        self.username = username
-        self.password = password
-
-    def __repr__(self):
-        return '<User %r>' % self.username
-
-
 
 
 #login check point decorator
--- models.py
+++ models.py
@@ -1,17 +1,21 @@
+#payments db 
+
+from app import *
+
 class PAYMENTS(db.Model):
     __tablename__ = 'PAYMENTS'
     id = db.Column(db.Integer, primary_key=True)
-    OBJECTID = db.Column(db.Integer)
-    Account =db.Column(db.String(60))
-    Amount_Due = db.Column(db.Integer)
+    Account = db.Column(db.Integer)
     GCR_No = db.Column(db.String(60))
     Payments = db.Column(db.Integer)
     PaymentType = db.Column(db.String(20))
-    DatePaid = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
     PaidBy = db.Column(db.String(60))
     PaidByTele = db.Column(db.String(10))
     Cashier = db.Column(db.String(25))
-    Date = db.Column(db.DateTime(), default=datetime.date.today())
+    Pc_name = db.Column(db.String(25))
+    DatePaid = db.Column(db.DateTime(), default=datetime.datetime.utcnow())
+    DatePaid = db.Column(db.DateTime(), default=datetime.datetime.today())
+
 
 
     def __init__(self, Account, GCR_No, Payments, PaymentType, PaidBy, PaidByTele, Cashier):
@@ -21,8 +25,24 @@ class PAYMENTS(db.Model):
         self.PaymentType = PaymentType
         self.PaidBy = PaidBy
         self.PaidByTele = PaidByTele
+        self.Pc_name = Pc_name
         self.Cashier = Cashier
-
 
     def __repr__(self):
         return '<records %r>' % self.Account
+
+#users db
+class User(db.Model):
+    __tablename__ = 'User'
+    #__table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(10), unique=True)
+    password = db.Column(db.String(10), unique=True)
+
+
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        return '<User %r>' % self.username
